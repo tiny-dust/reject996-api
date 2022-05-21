5bbadace972c4fd9b8769d3c97fff5f3579fd953
周顺顺 <idioticzhou@foxmail.com>
2022-05-21 16:37:15 +0800

    ✨ feat(package): 新增打包脚本

--- db/config.js
+++ db/config.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: '127.0.0.1',
+  // host: '127.0.0.1',
+  host: '150.158.181.254',
   user: 'root',
-  password: 'Zxcvb931224',
+  password: 'Zxcvb931224?',
   database: 'reject996',
 });
 

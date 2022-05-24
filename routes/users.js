4acd75f3188c581de97786867ec05dd5608b1e08
Zhoushunshun541 <idioticzhou@foxmail.com>
2022-05-24 10:16:49 +0800

    ✨ feat(登录接口):
    完成登录接口开发和token创建

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,13 @@
+const dayjs = require('dayjs');
 const express = require('express');
 const {
   sendEmail,
   getT,
 } = require('../utils/email');
+const {
+  connection,
+} = require('../db/config');
+const { createToken } = require('../utils/jwt');
 
 const router = express.Router();
 
@@ -26,12 +31,80 @@ router.get('/getCode', async (req, res) => {
   };
 });
 
-router.get('/demo', async (req, res) => {
-  res.send = {
-    code: 200,
-    data: '',
-    msg: 'success',
-  };
+router.post('/register', async (req, resp) => {
+  const { email, password } = req.body;
+  if (email !== '' && password !== '') {
+    const searchSql = `select count(*) from user where email = '${email}'`;
+    connection.query(searchSql, (err, res) => {
+      if (err) {
+        resp.send(err);
+        return;
+      }
+      if (res[0]['count(*)'] === 0) {
+        const sql = `insert into user (email, password,createTime) values('${email}','${password}','${dayjs().format('YYYY-MM-DD HH:mm:ss')}')`;
+        connection.query(sql, (error, r) => {
+          if (error) {
+            resp.send(error);
+            return;
+          }
+          resp.send({
+            code: 200,
+            msg: 'success',
+            data: r,
+          });
+        });
+      } else {
+        resp.send({
+          code: 403,
+          msg: '邮箱已注册',
+          data: '',
+        });
+      }
+    });
+  } else {
+    resp.send({
+      code: 403,
+      data: '',
+      msg: '邮箱或密码不能为空',
+    });
+  }
+});
+
+router.post('/login', (req, resp) => {
+  const { email, password } = req.body;
+  const sql = `select password, id from user where email = '${email}'`;
+  connection.query(sql, (err, r) => {
+    if (err) {
+      resp.send(err);
+      return;
+    }
+    if (r.length === 0) {
+      resp.send({
+        code: 202,
+        message: '查无此人',
+        data: '',
+      });
+      return;
+    }
+    if (r[0].password === password) {
+      const token = createToken(email, r[0].id);
+      resp.send({
+        code: 200,
+        msg: 'success',
+        data: {
+          token,
+          email,
+          user_id: r[0].id,
+        },
+      });
+    } else {
+      resp.send({
+        code: 403,
+        message: '密码错误',
+        data: '',
+      });
+    }
+  });
 });
 
 module.exports = router;
---
+++ utils/jwt.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+
+const privateKey = fs.readFileSync(path.join(__dirname, '../rsa/private_key.pem'));
+// const publicKey = fs.readFileSync(path.join(__dirname, '../rsa/public_key.pem'));
+exports.createToken = (email, id) => {
+  const payload = {
+    id,
+    email,
+  };
+  const options = {
+    expiresIn: '10h',
+    algorithm: 'RS256',
+  };
+  return jwt.sign(
+    payload,
+    privateKey,
+    options,
+  );
+};

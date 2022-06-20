2b9bf38d251c9d562a12e62c4f7c4d7230e0c3bf
周顺顺 <idioticzhou@foxmail.com>
2022-06-20 17:04:08 +0800

    🐛 fix(cros): 重新调整跨域资源访问

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors');
+const cors = require('koa2-cors');
 const { expressjwt: jwt } = require('express-jwt');
 const fs = require('fs');
 const rateLimit = require('express-rate-limit');
@@ -47,8 +47,8 @@ const codeLimit = rateLimit({
 });
 const app = express();
 
-app.use(logger('dev'));
 app.use(cors());
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
